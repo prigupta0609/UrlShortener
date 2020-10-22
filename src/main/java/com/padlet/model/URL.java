95f2bfc22d7d7c7767801134205d68760b5ed92f
Priyanka Gupta <priyankagupta0609@gmail.com>
2020-10-22 20:56:50 +0800

    Added domain layer model

--- src/main/java/com/padlet/model/URL.java
+++ src/main/java/com/padlet/model/URL.java
@@ -1,5 +1,32 @@
 package com.padlet.model;
 
+import java.io.Serializable;
+import java.util.Date;
+
 // Contains all information of URL and stored in DB
 public class URL {
+
+    private String shortUrl;
+    private String longUrl;
+
+    public URL(String shortUrl, String longUrl) {
+        this.shortUrl = shortUrl;
+        this.longUrl = longUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+
+    public String getLongUrl() {
+        return longUrl;
+    }
+
+    public void setLongUrl(String longUrl) {
+        this.longUrl = longUrl;
+    }
 }
